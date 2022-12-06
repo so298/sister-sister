d74306697c05c293dceed13a9f9f2bd3cff52dfe
Sosuke Hosokawa <so298go@gmail.com>
2022-12-06 15:54:04 +0900

    get urls of child articles

---
+++ scraping/wikipedia-en/scrape.py
@@ -0,0 +1,14 @@
+from bs4 import BeautifulSoup
+import requests
+import re
+
+
+root_url = "https://en.wikipedia.org/wiki/Lists_of_twin_towns_and_sister_cities"
+
+root_soup = BeautifulSoup(requests.get(root_url).content, 'html.parser')
+
+children_elems = root_soup.find_all(
+    "a", attrs={"href": re.compile(r"wiki/List_of_\w+$")})
+children_urls = ["https://en.wikipedia.org" +
+                 child_elem.get('href') for child_elem in children_elems]
+print(*children_urls, sep="\n")
