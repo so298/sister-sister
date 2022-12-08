d5bb6217109cedf44c97840dafff9226d998eb88
Sosuke Hosokawa <so298go@gmail.com>
2022-12-08 16:00:09 +0900

    scrape urls

--- scraping/wikipedia-en/scrape.py
+++ scraping/wikipedia-en/scrape.py
@@ -1,10 +1,16 @@
 from bs4 import BeautifulSoup, Tag, NavigableString
+import sys
 import requests
 import re
 from copy import deepcopy
+import json
+import time
+import pathlib
+
 
 def get_full_wikipedia_url(subpath: str):
     return "https://en.wikipedia.org" + subpath
+
 
 root_url = "https://en.wikipedia.org/wiki/Lists_of_twin_towns_and_sister_cities"
 
@@ -12,8 +18,10 @@ root_soup = BeautifulSoup(requests.get(root_url).content, 'html.parser')
 
 children_elems = root_soup.find_all(
     "a", attrs={"href": re.compile(r"wiki/List_of_[\w()]+$")})
-children_urls = [get_full_wikipedia_url(child_elem.get('href')) for child_elem in children_elems]
+children_urls = [get_full_wikipedia_url(
+    child_elem.get('href')) for child_elem in children_elems]
 # print(*children_urls, sep="\n")
+
 
 def read_child_page(url: str):
     soup = BeautifulSoup(requests.get(url).content, "html.parser")
@@ -33,14 +41,14 @@ def read_child_page(url: str):
                 continue
             full_href = get_full_wikipedia_url(href)
             if turn % 2 == 0:
-                city['country'] = full_href
+                city["country_name"] = link.get("title")
+                city['country_url'] = full_href
             else:
-                city['name'] = full_href
+                city["city_name"] = link.text
+                city['city_url'] = full_href
                 parsed.append(deepcopy(city))
             turn += 1
         return parsed
-
-
 
     for elem in main_content.children:
         inner_soup = BeautifulSoup(str(elem), 'html.parser')
@@ -51,14 +59,38 @@ def read_child_page(url: str):
             link = inner_soup.p.b.a
             city_name = link.text
             city_url = get_full_wikipedia_url(link.get('href'))
-            print(city_name, city_url)
+            print(city_name, city_url, file=sys.stderr)
         elif ul != None:
-            print(parse_ul(inner_soup.ul))
+            output.append({
+                "city_name": city_name,
+                "city_url": city_url,
+                "sister": parse_ul(inner_soup.ul)
+            })
 
         elif div != None:
-            if not "div-col" in div.get("class"):
+            if not div.get("class") or not "div-col" in div.get("class"):
                 continue
-            print(parse_ul(inner_soup.div.ul))
-    
+            output.append({
+                "city_name": city_name,
+                "city_url": city_url,
+                "sister": parse_ul(inner_soup.div.ul)
+            })
+
     return output
-read_child_page(children_urls[0])
+
+
+# print(json.dumps(read_child_page(children_urls[0])))
+
+all_info = []
+for child_url in children_urls:
+    print(child_url, file=sys.stderr)
+    all_info.append({
+        "page_url": child_url,
+        "content": read_child_page(child_url)
+    })
+    time.sleep(0.1)
+
+    url_path = pathlib.Path(child_url)
+    with open(f"cache/{url_path.name}.json", "w") as f:
+        print(f"\033[32m save to {f'cache/{url_path.name}.json'}\033[0m", file=sys.stderr)
+        json.dump(all_info, f)
