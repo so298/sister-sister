15f3d91f42070f084ca4c4f0075bc309d79f836c
Kosei Nakamura <nakamura-3k559111@g.ecc.u-tokyo.ac.jp>
2022-12-12 14:39:06 +0900

    develop structure

--- frontend/src/pages/components/Home/index.tsx
+++ frontend/src/pages/components/Home/index.tsx
@@ -1,11 +1,13 @@
 import { FC } from 'react';
 
 import DrawerSection from './DrawerSection';
+import HeaderSection from './HeaderSection';
 
 export const Home: FC = () => {
   // const [opened, setOpened] = useState<boolean>(false);
   return (
     <>
+      <HeaderSection />
       <DrawerSection />
     </>
   );
