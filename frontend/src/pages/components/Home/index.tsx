bdb3677c03b6d0823dbea6ac6cbfb3b95f06bb3f
naga1ay <nagai-ayumu471@g.ecc.u-tokyo.ac.jp>
2022-12-12 14:40:26 +0900

    Card initial

--- frontend/src/pages/components/Home/index.tsx
+++ frontend/src/pages/components/Home/index.tsx
@@ -1,12 +1,14 @@
 import { FC } from 'react';
 
 import DrawerSection from './DrawerSection';
+import RightCardSection from './RightCardsSection';
 
 export const Home: FC = () => {
   // const [opened, setOpened] = useState<boolean>(false);
   return (
     <>
       <DrawerSection />
+      <RightCardSection />
     </>
   );
 };
