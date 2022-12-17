e76186391530d04a13b6819f3291fe7f6736965d
Sosuke Hosokawa <so298go@gmail.com>
2022-12-17 16:21:40 +0900

    build graph

--- scraping/wikipedia-en/scrape_all_cities.py
+++ scraping/wikipedia-en/scrape_all_cities.py
@@ -1,5 +1,6 @@
 import json
 import sys
+from copy import deepcopy
 
 from read_page import read_page_a
 from util import print_color_url
@@ -31,13 +32,31 @@ for country in countries:
         '''
         city_node = {
             'name': city['city_name'],
+            'country_name': country,
             'url': city['city_url'],
-            'sisters': []
+            'sisters': set()
         }
 
         for sister in city['sister']:
-            city_node['sisters'].append(sister['city_url'])
+            city_node['sisters'].add(sister['city_url'])
         
         sis_graph[city_node['url']] = city_node
         
-    print(sis_graph)
+    # print(sis_graph)
+
+# make two-way edged
+sis_graph_cp = deepcopy(sis_graph)
+for key_url, node in sis_graph.items():
+    for sister_url in node['sisters']:
+        if sister_url in sis_graph_cp.keys():
+            # I didn't scrape philipines page, so there may be non-exists node
+            sis_graph_cp[sister_url]['sisters'].add(key_url)
+
+sis_graph = sis_graph_cp
+
+# change set to list
+for node in sis_graph.values():
+    node['sisters'] = list(node['sisters'])
+
+with open("data/city_graph.json", "w") as f:
+    json.dump(sis_graph, f)
