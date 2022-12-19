4d034606b9482a8e78d8da1b22260d23fea799c1
Sosuke Hosokawa <so298go@gmail.com>
2022-12-19 15:27:45 +0900

    update image

--- scraping/wikipedia-en/temp.py
+++ scraping/wikipedia-en/temp.py
@@ -1,9 +1,9 @@
 import json
 
-with open("data/city_data_all.json") as f:
+with open("data/city_data_all_image.json") as f:
     data = json.load(f)
 
-with open("data/city_data_all_image.json") as f:
+with open("data/city_data_all_image_temp.json") as f:
     data_image = json.load(f)
 
 for i, city in enumerate(data_image):
